56041e5725d7dbdf99916f286d5ea3cb10385d6f
martinsmandina <martinsmandina@gmail.com>
2020-11-22 19:08:14 +0300

    create UpdateProfileForm

---
+++ instaapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Image,Profile,Comments
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
--- instaapp/views.py
+++ instaapp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse,Http404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
+from .forms import UpdateProfileForm
 
 
 # Create your views here.
@@ -21,8 +22,23 @@ def profile(request):
     profile = Profile.get_profile()
     image = Image.images_get_all()
     comments = Comment.comments_get_all()
-    return render(request,'profile/profile.html',{"title":title,"comments":comments,"profile":profile,"image":image,"user":user})
-                                                  
+    return render(request,'profile/profile.html',{"comments":comments,"profile":profile,"image":image,"user":user})
+
+ @login_required(login_url='/accounts/login/')
+def edit(request):
+    title = 'Insta-Gram'
+    current_user = request.user
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            update = form.save(commit=False)
+            update.user = current_user
+            update.save()
+            return redirect('profile')
+    else:
+        form = EditProfileForm()
+    return render(request,'profile/update.html',{"form":form}) 
+                                                
                                                   
                                                   
                                                   
