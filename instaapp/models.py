6e855f7500ea735076dee8de931359fd4729232c
martinsmandina <martinsmandina@gmail.com>
2020-11-22 18:30:12 +0300

    add view main

--- instaapp/models.py
+++ instaapp/models.py
@@ -5,8 +5,8 @@ class Profile(models.Model):
     profilePhoto =  models.ImageField(upload_to='profile/',null=True,blank=True)
     bio = models.CharField(max_length=60,blank=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-
-
+ 
+ 
     def __str__(self):
         return self.bio 
 
--- instaapp/views.py
+++ instaapp/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth import login, authenticate
 # Create your views here.
 @login_required(login_url='/')
 def main(request):
+    current_user = request.user
+    profile = Profile.get_profile()
     images = Image.images_get_all()
-    return render(request, 'index.html', {'images':images})
+    comments = Comments.comments_get_all()
+    return render(request, 'index.html', {'images':images,"profile":profile,"current_user":current_user,"comments":comments})
 
