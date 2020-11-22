0c66ec93d98be63213fa1a3fdd612bc7777ff13d
martinsmandina <martinsmandina@gmail.com>
2020-11-22 20:23:14 +0300

    create media file

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -136,6 +137,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Static files (CSS, JavaScript, Images)
