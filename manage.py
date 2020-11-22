0c4465086062b7481d553acb1d6aca446979d407
martinsmandina <martinsmandina@gmail.com>
2020-11-22 14:46:41 +0300

    start app

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Instaapp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instaproject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
