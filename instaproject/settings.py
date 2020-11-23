3f588cacb2fffa125db973d8c5d11d462ec7314b
martinsmandina <martinsmandina@gmail.com>
2020-11-23 12:27:05 +0300

    add email config to settings

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -147,3 +147,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+
